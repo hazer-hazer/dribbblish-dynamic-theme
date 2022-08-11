c66d2da80b8bd08ec0b5089814ec205727e0ddc9
hazer-hazer <gordeev.john@gmail.com>
2022-08-11 22:51:29 +0300

    not so kek

--- install_local.sh
+++ install_local.sh
@@ -1,4 +1,7 @@
 npm run build
 cp ./dist/dribbblish-dynamic.js %appdata%/spicetify/Extensions/
 spicetify config extensions dribbblish-dynamic.js
+spicetify config current_theme DribbblishDynamic
+spicetify config color_scheme base
+spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
 spicetify apply
